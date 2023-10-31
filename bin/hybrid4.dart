bf86ef96ff989c379fee1d1a4bbd8a64798d8ff5
Luciano Saavedra <saav0004@algonquinlive.com>
2023-10-31 16:06:14 -0400

    initial commit

---
+++ bin/hybrid4.dart
@@ -0,0 +1,5 @@
+import 'package:hybrid4/hybrid4.dart' as hybrid4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid4.calculate()}!');
+}
---
+++ test/hybrid4_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid4/hybrid4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
