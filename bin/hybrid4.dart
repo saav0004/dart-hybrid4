24386de8dd570df57ee216a6bc076014b59f9002
Luciano Saavedra <saav0004@algonquinlive.com>
2023-10-31 18:16:40 -0400

    fetch users

--- bin/hybrid4.dart
+++ bin/hybrid4.dart
@@ -1,5 +1,32 @@
-import 'package:hybrid4/hybrid4.dart' as hybrid4;
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${hybrid4.calculate()}!');
+import 'package:hybrid4/hybrid4.dart' as hybrid4;
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() {
+  fetch().then((value) {
+    // print value
+    for (var item in value) {
+      var firstName = item['first_name'];
+      var lastName = item['last_name'];
+      var id = item['uid'];
+      print("\nFirst Name: $firstName \nLast Name: $lastName \nID: $id\n");
+    }
+  });
+}
+
+// Fetch function
+
+Future<List<Map<String, dynamic>>> fetch() async {
+  var url = "https://random-data-api.com/api/v2/users?size=2";
+  // transform url to uri
+  var uri = Uri.parse(url);
+  // get responses
+  var response = await http.get(uri);
+  // get body
+  var body = response.body;
+  var data = convert.jsonDecode(body);
+
+  return data.cast<Map<String, dynamic>>();
 }
