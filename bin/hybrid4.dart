3f926a515376e74daebc7befe320248f0a850e47
Luciano Saavedra <saav0004@algonquinlive.com>
2023-10-31 18:42:00 -0400

    handle error handling

--- bin/hybrid4.dart
+++ bin/hybrid4.dart
@@ -4,16 +4,19 @@ import 'package:hybrid4/hybrid4.dart' as hybrid4;
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
-void main() {
-  fetch().then((value) {
-    // print value
-    for (var item in value) {
+void main() async {
+  try {
+    var data = await fetch();
+
+    for (var item in data) {
       var firstName = item['first_name'];
       var lastName = item['last_name'];
       var id = item['uid'];
       print("\nFirst Name: $firstName \nLast Name: $lastName \nID: $id\n");
     }
-  });
+  } catch (e) {
+    print("Error: $e");
+  }
 }
 
 // Fetch function
@@ -22,8 +25,14 @@ Future<List<Map<String, dynamic>>> fetch() async {
   var url = "https://random-data-api.com/api/v2/users?size=2";
   // transform url to uri
   var uri = Uri.parse(url);
+
   // get responses
   var response = await http.get(uri);
+
+  //error handler
+  if (response.statusCode != 200) {
+    throw Exception("Error: ${response.statusCode}");
+  }
   // get body
   var body = response.body;
   var data = convert.jsonDecode(body);
