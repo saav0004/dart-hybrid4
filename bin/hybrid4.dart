dacce13ef0a4f49fb04785049a4f8242ed824d56
Luciano Saavedra <saav0004@algonquinlive.com>
2023-11-01 13:54:54 -0400

    Wrap async await in try catch

--- bin/hybrid4.dart
+++ bin/hybrid4.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:hybrid4/hybrid4.dart' as hybrid4;
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
@@ -12,7 +10,7 @@ void main() async {
       var firstName = item['first_name'];
       var lastName = item['last_name'];
       var id = item['uid'];
-      print("\nFirst Name: $firstName \nLast Name: $lastName \nID: $id\n");
+      print("\nFirst Name: $firstName \nLast Name: $lastName \nID: $id");
     }
   } catch (e) {
     print("Error: $e");
@@ -20,20 +18,14 @@ void main() async {
 }
 
 // Fetch function
-
 Future<List<Map<String, dynamic>>> fetch() async {
-  var url = "https://random-data-api.com/api/v2/users?size=2";
-  // transform url to uri
+  var url = "https://random-data-api.com/api/v2/users?size=10";
   var uri = Uri.parse(url);
 
-  // get responses
   var response = await http.get(uri);
-
-  //error handler
   if (response.statusCode != 200) {
     throw Exception("Error: ${response.statusCode}");
   }
-  // get body
   var body = response.body;
   var data = convert.jsonDecode(body);
 
